88f25a701d0fdabe8a8a6eb48ac30c3ca3d31a22
Felipe Ramirez <feliperamirezg@gmail.com>
2018-07-28 17:52:41 -0400

    Removes grunt copy task; improves image presentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,61 +2,6 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // Copy web assets from bower_components to more convenient directories.
-    copy: {
-      main: {
-        files: [
-          // Vendor scripts.
-          {
-            expand: true,
-            cwd: 'bower_components/bootstrap-sass/assets/javascripts/',
-            src: ['**/*.js'],
-            dest: 'www/js/bootstrap-sass/'
-          },
-          {
-            expand: true,
-            cwd: 'bower_components/jquery/dist/',
-            src: ['**/*.js', '**/*.map'],
-            dest: 'www/js/jquery/'
-          },
-
-          // Fonts.
-          {
-            expand: true,
-            filter: 'isFile',
-            flatten: true,
-            cwd: 'bower_components/',
-            src: ['bootstrap-sass/assets/fonts/**'],
-            dest: 'www/fonts/'
-          },
-
-          // Stylesheets
-          {
-            expand: true,
-            cwd: 'bower_components/bootstrap-sass/assets/stylesheets/',
-            src: ['**/*.scss'],
-            dest: 'scss/'
-          }
-        ]
-      },
-    },
-
-    // Compile SASS files into minified CSS.
-    sass: {
-      options: {
-        includePaths: ['bower_components/bootstrap-sass/assets/stylesheets'],
-        livereload: true
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
-        },
-        files: {
-          'www/css/bootstrap-agronomic.css': 'scss/app.scss'
-        }
-      }
-    },
-
     // Watch these files and notify of changes.
     watch: {
       options: {
@@ -87,17 +32,17 @@ module.exports = function (grunt) {
         }
       }
     }
-  });
+  })
 
   // Load externally defined tasks.
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-sass')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-connect')
 
   // Establish tasks we can run from the terminal.
-  grunt.registerTask('build', ['sass', 'copy']);
-  grunt.registerTask('default', ['build', 'watch']);
-  grunt.registerTask('serve', ['build', 'connect', 'watch']);
-  grunt.registerTask('heroku', ['build']);
+  grunt.registerTask('build', [])
+  grunt.registerTask('default', ['build', 'watch'])
+  grunt.registerTask('serve', ['build', 'connect', 'watch'])
+  grunt.registerTask('heroku', ['build'])
 }
